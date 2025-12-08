705023525e4b79d45f9d851b28e671d2501fb61e
Kiia Mäntylä <kiia.mantyla@helsinki.fi>
2025-12-08 15:37:58 +0200

    added laskin

---
+++ viikko6/laskin/src/erotus.py
@@ -0,0 +1,16 @@
+class Erotus:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+        self._edellinen_arvo = 0
+
+    def suorita(self):
+        self._edellinen_arvo = self._sovelluslogiikka.arvo()
+        try:
+            luku = int(self._lue_syote())
+            self._sovelluslogiikka.miinus(luku)
+        except:
+            pass
+
+    def kumoa(self):
+        self._sovelluslogiikka.aseta_arvo(self._edellinen_arvo)
---
+++ viikko6/laskin/src/nollaus.py
@@ -0,0 +1,12 @@
+class Nollaus:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+        self._edellinen_arvo = 0
+
+    def suorita(self):
+        self._edellinen_arvo = self._sovelluslogiikka.arvo()
+        self._sovelluslogiikka.nollaa()
+
+    def kumoa(self):
+        self._sovelluslogiikka.aseta_arvo(self._edellinen_arvo)
