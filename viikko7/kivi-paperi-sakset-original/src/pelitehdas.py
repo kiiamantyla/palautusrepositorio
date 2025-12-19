bd4d5178854d6013ce945ae182875a733b94ac03
Kiia Mäntylä <kiia.mantyla@helsinki.fi>
2025-12-19 11:09:10 +0200

    tallennettiin koodi

---
+++ viikko7/kivi-paperi-sakset-original/src/pelitehdas.py
@@ -0,0 +1,12 @@
+from kps_pvp import KPSPelaajaVsPelaaja
+from kps_tekoaly import KPSTekoaly
+from kps_parempi_tekoaly import KPSParempiTekoaly
+
+def luo_peli(valinta):
+    if valinta == "a":
+        return KPSPelaajaVsPelaaja()
+    if valinta == "b":
+        return KPSTekoaly()
+    if valinta == "c":
+        return KPSParempiTekoaly()
+    return None
